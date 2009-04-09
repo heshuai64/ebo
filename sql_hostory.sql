69f831172dfbca6838d702f23101e87734a0effb
heshuai64 <heshuai64@8678416c-2391-11de-982c-e375d58577e7>
2009-04-09 01:21:41 +0000

    eBay proxy manage

--- sql_hostory.sql
+++ sql_hostory.sql
@@ -40,3 +40,100 @@ NULL , '6', '37'
 );
 
 ----------------------------------------------------------------------------------------------------
+
+
+  INSERT INTO `ebaybo`.`qo_modules_actions` (
+        `id` ,
+        `qo_modules_id` ,
+        `name` ,
+        `description`
+        )
+        VALUES (
+        NULL , '9', 'getAllEbaySeller', '获取所有eBay账户信息'
+        );
+        
+        INSERT INTO `ebaybo`.`qo_privileges_has_module_actions` (
+        `id` ,
+        `qo_privileges_id` ,
+        `qo_modules_actions_id`
+        )
+        VALUES (
+        NULL , '3', '36'
+----------------------------------------------------------------------------------------------------
+
+   INSERT INTO `ebaybo`.`qo_modules_actions` (
+        `id` ,
+        `qo_modules_id` ,
+        `name` ,
+        `description`
+        )
+        VALUES (
+        NULL , '9', 'updateEbaySeller', '更新eBay账户信息'
+        ), (
+        NULL , '9', 'deleteEbaySeller', '删除eBay账户'
+        );
+        
+        INSERT INTO `ebaybo`.`qo_privileges_has_module_actions` (
+        `id` ,
+        `qo_privileges_id` ,
+        `qo_modules_actions_id`
+        )
+        VALUES (
+        NULL , '3', '37'
+        ), (
+        NULL , '3', '38'
+        );
+-------------------------------------------------------------------------------------------------------
+
+ CREATE TABLE `ebaybo`.`qo_ebay_proxy` (
+`id` INT NOT NULL ,
+`ebay_seller_id` VARCHAR( 60 ) NOT NULL ,
+`proxy_host` VARCHAR( 200 ) NOT NULL ,
+`proxy_port` INT NOT NULL
+) ENGINE = MYISAM 
+
+-------------------------------------------------------------------------------------------------------
+
+
+INSERT INTO `ebaybo`.`qo_modules_actions` (
+`id` ,
+`qo_modules_id` ,
+`name` ,
+`description`
+)
+VALUES (
+NULL , '9', 'addEbaySeller', '添加eBay帐号'
+), (
+NULL , '9', NULL , NULL
+), (
+NULL , '9', 'getAllEbayProxy', '获取所有eBay代理'
+), (
+NULL , '9', 'addEbayProxy', '添加eBay代理'
+), (
+NULL , '9', 'updateEbayProxy', '更新eBay代理'
+), (
+NULL , '9', 'deleteEbayProxy', '更新eBay代理'
+);
+
+
+INSERT INTO `ebaybo`.`qo_privileges_has_module_actions` (
+`id` ,
+`qo_privileges_id` ,
+`qo_modules_actions_id`
+)
+VALUES (
+NULL , '3', '41'
+), (
+NULL , '3', '42'
+), (
+NULL , '3', '43'
+), (
+NULL , '3', '44'
+), (
+NULL , '3', '45'
+), (
+NULL , '3', '46'
+);
+
+
+-------------------------------------------------------------------------------------------------------
--- system/modules/manage/manage-override.js
+++ system/modules/manage/manage-override.js
@@ -533,7 +533,7 @@ Ext.override(QoDesk.Manage, {
 			frame: true,
 			labelAlign: 'left',
 			bodyStyle:'padding:5px',
-			labelWidth:80,
+			labelWidth:75,
 			//width: 750,
 			layout:"column",
 			items:[{
@@ -620,7 +620,7 @@ Ext.override(QoDesk.Manage, {
 			    handler: function(){
 				 Ext.Ajax.request({
 				    waitMsg: 'Please wait...',
-				    url: 'connect.php?moduleId=qo-orders&action=saveEbaySeller',
+				    url: 'connect.php?moduleId=qo-orders&action=updateEbaySeller',
 				    params: {
 					    id: ebayManageForm.form.findField('id').getValue(),
 					    email: ebayManageForm.form.findField('email').getValue(),
@@ -739,7 +739,7 @@ Ext.override(QoDesk.Manage, {
 		ebayManageWin = desktop.createWindow({
 		    id: 'ebay-manage-win',
 		    title:'eBay账户管理',
-		    width:700,
+		    width:720,
 		    height:500,
 		    iconCls: 'ebay-manage-icon',
 		    shim:false,
@@ -751,6 +751,147 @@ Ext.override(QoDesk.Manage, {
 		});
 	    }
 	    ebayManageWin.show();
+	}
+	
+	var ebayProxyManage = function(){
+	    var ebayProxyManageWin = desktop.getWindow('ebay-proxy-manage-win');
+	    
+	    if(!ebayProxyManageWin){
+		var store = new Ext.data.JsonStore({
+		    root: 'result',
+		    autoLoad: true,
+		    fields: ['id','ebay_seller_id','proxy_host','proxy_port'],
+		    url:'connect.php?moduleId=qo-manage&action=getAllEbayProxy'
+		});
+		
+		var notifyWin = desktop.showNotification({
+				    html: '加载用户信息中...', 
+				    title: '请等待'
+				});
+		
+		saveComplete(notifyWin, '完成', '加载用户信息成功.');
+		
+		
+		var gridForm = new Ext.FormPanel({
+		    id: 'ebay-proxy-manage-form',
+		    frame: true,
+		    labelAlign: 'left',
+		    //title: '用户管理',
+		    bodyStyle:'padding:5px',
+		    //width: 750,
+		    layout: 'column',	
+		    items: [{
+			columnWidth: 0.55,
+			layout: 'fit',
+			items: {
+				xtype: 'grid',
+				store: store,
+				columns:[
+					{header: "eBay帐号", width: 135, sortable: true,  dataIndex: 'ebay_seller_id'},
+					{header: "代理主机", width: 125, sortable: true, dataIndex: 'proxy_host'},
+					{header: "代理端口", width: 40, sortable: true, dataIndex: 'proxy_port'}
+				    ],
+				sm: new Ext.grid.RowSelectionModel({
+				    singleSelect: true,
+				    listeners: {
+					rowselect: function(sm, row, rec) {
+					    Ext.getCmp("ebay-proxy-manage-form").getForm().loadRecord(rec);
+					}
+				    }
+				}),
+				height: 350,
+				title:'用户列表',
+				border: true,
+				listeners: {
+					render: function(g) {
+						g.getSelectionModel().selectRow(0);
+					},
+					delay: 10 // Allow rows to be rendered.
+				}
+			    }
+		    },{
+			columnWidth: 0.45,
+			xtype: 'fieldset',
+			labelWidth: 60,
+			//title:'用户信息',
+			defaults: {width: 130},	// Default config options for child items
+			defaultType: 'textfield',
+			autoHeight: true,
+			bodyStyle: Ext.isIE ? 'padding:0 0 5px 15px;' : 'padding:10px 15px;',
+			border: false,
+			style: {
+			    "margin-left": "10px", // when you add custom margin in IE 6...
+			    "margin-right": Ext.isIE6 ? (Ext.isStrict ? "-10px" : "-13px") : "0"  // you have to adjust for it somewhere else
+			},
+			items: [{
+			    fieldLabel: 'eBay帐号',
+			    name: 'ebay_seller_id'
+			},{
+			    fieldLabel: '代理主机',
+			    name: 'proxy_host'
+			},{
+			    fieldLabel: '代理端口',
+			    name: 'proxy_port'
+			}]
+		    }],
+		    buttons: [{
+			text: '添加eBay代理',
+			handler: function(){
+			    
+			}
+		    },{
+			text: '保存选中的eBay代理',
+			handler: function(){
+			    var notifyWin = desktop.showNotification({
+				    html: '保存用户信息中...'
+				    , title: '请等待'
+			    });
+			    var memberForm = Ext.getCmp("user-manage-form").getForm();
+			    
+			    Ext.Ajax.request({
+				url: "connect.php",
+				params: "moduleId=qo-manage&action=updateMember&id="+memberForm.items.items[0].value+"&email_address="+memberForm.items.items[1].value+"&password="+memberForm.items.items[2].value+"&active="+memberForm.items.items[3].value,
+				success: function(o){
+				    saveComplete(notifyWin, '完成', '保存用户信息成功.');
+				},
+				failure: function(){
+					    saveComplete(notifyWin, 'Error', 'Lost connection to server.');
+					},
+					scope: this
+			    });
+			}
+		    },{
+			text: '删除选中的eBay代理',
+			handler: function(){
+			    
+			}
+		    },{
+			text: '关闭窗口',
+			handler: function(){
+			    ebayProxyManageWin.close();
+			}
+		    }]
+		});
+			    
+		ebayProxyManageWin = desktop.createWindow({
+				    id: 'ebay-proxy-manage-win',
+				    title:'eBay代理管理',
+				    width:600,
+				    height:500,
+				    iconCls: 'ebay-proxy-icon',
+				    shim:false,
+				    animCollapse:false,
+				    constrainHeader:true,
+				    layout: 'fit',
+				    items: gridForm,
+				    taskbuttonTooltip: '<b>eBay代理管理</b><br />设置eBay帐户获取订单所经过的代理服务器'
+				});
+				
+		ebayProxyManageWin.show();
+	
+	    }else{
+		ebayProxyManageWin.show();
+	    }
 	}
 	
         if(!win){        	
@@ -778,6 +919,7 @@ Ext.override(QoDesk.Manage, {
 	    //Ext.EventManager.on(Ext.DomQuery.select("div[@class='privilege-manage']")[0], "click", privilegeManage);
 	    Ext.EventManager.on(Ext.DomQuery.select("div[@class='group-privilege-manage']")[0], "click", groupPrivilegeManage);
 	    Ext.EventManager.on(Ext.DomQuery.select("div[@class='ebay-manage']")[0], "click", ebayManage);
+	    Ext.EventManager.on(Ext.DomQuery.select("div[@class='ebay-proxy']")[0], "click", ebayProxyManage);
         }
         
         win.show();
