72d535290e593ec5190b6b9aba18ae0ca7de8e27
heshuai64 <heshuai64@8678416c-2391-11de-982c-e375d58577e7>
2009-04-14 18:48:31 +0000

    reports bug

--- trunk/system/modules/reports/reports-override.js
+++ trunk/system/modules/reports/reports-override.js
@@ -8,87 +8,83 @@ Ext.override(QoDesk.Reports, {
 	    window.open("http://127.0.0.1:6666/eBayBO/reports.php?type=skuSell&seller_id="+seller_id+"&start_date="+start_date+"&end_date="+end_date,"_blank","toolbar=no, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=10, height=10");
 	}
 	
-        if(!win){
-	    
-	    
-	    var skuSellReport = function(){
-		var skuSellReportWin = desktop.getWindow('sku-sell-win');
-		if(!skuSellReportWin){
-		    skuSellReportWin = desktop.createWindow({
-			id: 'sku-sell-win',
-			title:lang.reports.search_sku_sell_window_title,
-			width:300,
-			height:200,
-			iconCls: 'sku-sell-icon',
-			shim:false,
-			animCollapse:false,
-			constrainHeader:true,
-			layout: 'fit',
+	var skuSellReport = function(){
+	    var skuSellReportWin = desktop.getWindow('sku-sell-win');
+	    if(!skuSellReportWin){
+		skuSellReportWin = desktop.createWindow({
+		    id: 'sku-sell-win',
+		    title:lang.reports.search_sku_sell_window_title,
+		    width:300,
+		    height:200,
+		    iconCls: 'sku-sell-icon',
+		    shim:false,
+		    animCollapse:false,
+		    constrainHeader:true,
+		    layout: 'fit',
+		    //html:'test',
+		    items:[{
+			xtype:"form",
+			labelWidth:80,
 			items:[{
-			    xtype:"form",
-			    labelWidth:80,
-			    default:{
-				width:150
-			    },
-			    items:[{
-				id:'sellerId',
-				xtype: 'combo',
-				fieldLabel:lang.reports.seller_id,
-				mode: 'local',
-				store: new Ext.data.JsonStore({
-				    autoLoad: true,
-				    fields: ['id', 'name'],
-				    url: "connect.php?moduleId=qo-transactions&action=getSeller"
-				}),
-				valueField:'id',
-				displayField:'name',
-				triggerAction: 'all',
-				editable: false,
-				selectOnFocus:true,
-				width:150,
-				listWidth:150,
-				name: 'sellerId',
-				hiddenName:'sellerId'
-			    },{
-				id:'start_date',
-				xtype:"datefield",
-				fieldLabel:lang.reports.start_date,
-				format:'Y-m-d',
-				name:"start_date"
-			    },{
-				id:'end_date',
-				xtype:"datefield",
-				fieldLabel:lang.reports.end_date,
-				format:'Y-m-d',
-				name:"end_date"
-			    }],
-			    buttons: [{
-                                text: lang.reports.submit,
-                                handler: function(){
-				    generateSkuSellReport(document.getElementById("sellerId").value, document.getElementById("start_date").value,document.getElementById("end_date").value);
-				    skuSellReportWin.close();
-                                }
-                            },{
-				text: lang.reports.close,
-                                handler: function(){
-                                    skuSellReportWin.close();
-                                }	
-			    }]
+			    id:'sellerId',
+			    xtype: 'combo',
+			    fieldLabel:lang.reports.seller_id,
+			    mode: 'local',
+			    store: new Ext.data.JsonStore({
+				autoLoad: true,
+				fields: ['id', 'name'],
+				url: "connect.php?moduleId=qo-transactions&action=getSeller"
+			    }),
+			    valueField:'id',
+			    displayField:'name',
+			    triggerAction: 'all',
+			    editable: false,
+			    selectOnFocus:true,
+			    width:150,
+			    listWidth:150,
+			    name: 'sellerId',
+			    hiddenName:'sellerId'
+			},{
+			    id:'start_date',
+			    xtype:"datefield",
+			    fieldLabel:lang.reports.start_date,
+			    format:'Y-m-d',
+			    name:"start_date"
+			},{
+			    id:'end_date',
+			    xtype:"datefield",
+			    fieldLabel:lang.reports.end_date,
+			    format:'Y-m-d',
+			    name:"end_date"
 			}],
-			taskbuttonTooltip: '<b>统计报告</b><br />各种不同的统计报告'
-		    });
-		    skuSellReportWin.show();
-		}else{
-		    skuSellReportWin.show();
-		}
+			buttons: [{
+			    text: lang.reports.submit,
+			    handler: function(){
+				generateSkuSellReport(document.getElementById("sellerId").value, document.getElementById("start_date").value,document.getElementById("end_date").value);
+				skuSellReportWin.close();
+			    }
+			},{
+			    text: lang.reports.close,
+			    handler: function(){
+				skuSellReportWin.close();
+			    }
+			}]
+		    }],
+		    taskbuttonTooltip: '<b>统计报告</b><br />各种不同的统计报告'
+		});
+		
 	    }
+	    
+	    skuSellReportWin.show();
+	}
 	
+	if(!win){
             win = desktop.createWindow({
                 id: 'reports-win',
                 title:lang.reports.window_title,
                 width:340,
                 height:200,
-                iconCls: 'reports-icon',
+                //iconCls: 'reports-icon',
                 shim:false,
                 animCollapse:false,
                 constrainHeader:true,
