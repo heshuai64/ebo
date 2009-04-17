803f3cc774b06407dfd1fed81725d59a61092eb5
heshuai64 <heshuai64@8678416c-2391-11de-982c-e375d58577e7>
2009-04-17 08:58:19 +0000

    javasript compressor

---
+++ system/modules/shipments/verify-shipment.js
@@ -0,0 +1,81 @@
+Ext.onReady(function(){
+        var verifyshipmentStore = new Ext.data.JsonStore({
+            root: 'records',
+            totalProperty: 'totalCount',
+            idProperty: 'id',
+            fields: ['id', 'skuId', 'skuTitle', 'itemId', 'itemTitle', 'quantity'],
+            url:'connect.php?moduleId=qo-shipments&action=verifyShipment'
+        });
+        
+        function renderSkuImage(v, p, r){
+            return String.format('<img src="http://m2.sourcingmap.com/smap/images/item/medium/ux_{0}_ux_m.jpg"', v);
+        }
+        
+        function renderShipmentInfo(v, p, r){
+            return String.format(' X {0} <br><br> {1} <br><br> {2}', r.data.quantity , r.data.skuId , r.data.skuTitle);
+        }
+        
+        var verifyshipmentGrid = new Ext.grid.EditorGridPanel({
+                autoHeight: true,
+                store: verifyshipmentStore,
+                selModel: new Ext.grid.RowSelectionModel({}),
+                columns:[{
+                    header: "Image",
+                    dataIndex: 'skuId',
+                    renderer: renderSkuImage,
+                    width: 105,
+                    align: 'center'
+                },{
+                    header: "Info",
+                    dataIndex: 'skuId',
+                    renderer: renderShipmentInfo,
+                    width: 500,
+                    align: 'center'
+                }]
+        });
+                
+        var verifyShipmentForm = new Ext.FormPanel({
+            id:"verify-shipment-form",
+            autoScroll:true,
+            title:"Verify Shipment",
+            items:[{
+                    layout:"column",
+                    items:[{
+                        columnWidth:0.3,
+                        layout:"form",
+                        border:false,
+                        items:[{
+                            xtype:"textfield",
+                            fieldLabel:"ShipmentId",
+                            name:"id",
+                            listeners:{specialkey: function(t, e){
+                                    if(e.getKey() == 13){
+                                        verifyshipmentStore.load({params:{id:verifyShipmentForm.getForm().findField('id').getValue()}});
+                                        Ext.getCmp('verify-shipment-form').form.findField('id').focus(true);
+                                    }
+                                }
+                            }
+                        }]   
+                      },{
+                        columnWidth:0.7,
+                        layout:"form",
+                        border:false,
+                        items:[{
+                            xtype:"button",
+                            text:"Submit",
+                            handler: function(){
+                                verifyshipmentStore.load({params:{id:verifyShipmentForm.getForm().findField('id').getValue()}});
+                                Ext.getCmp('verify-shipment-form').form.findField('id').focus(true);
+                            }
+                          }]
+                      }]
+                    },{
+                        xtype: 'panel',
+                        autoHeight: true,
+                        items: verifyshipmentGrid
+                    }
+                ]
+        });
+        
+        verifyShipmentForm.render(document.body);
+})
