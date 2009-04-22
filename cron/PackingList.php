c77ec0ce054f50f29a9ab10493896e920f5b1231
heshuai64 <heshuai64@8678416c-2391-11de-982c-e375d58577e7>
2009-04-22 15:47:22 +0000

    cron script

---
+++ cron/PackingList.php
@@ -0,0 +1,146 @@
+<?php
+
+class PackingList{
+    private static $database_connect;
+    const DATABASE_HOST = 'localhost';
+    const DATABASE_USER = 'root';
+    const DATABASE_PASSWORD = '';
+    const DATABASE_NAME = 'ebaybo';
+    const FILE_PATH = '/export/eBayBO/packing/';
+    const BAR_CODE_URL = '/eBayBO/cron/image.php';
+    private $sellerSell = array();
+    private $sellSku = array();
+    private $startTime;
+    private $endTime;
+    private $shipment = array();
+    
+    public function __construct(){
+        PackingList::$database_connect = mysql_connect(self::DATABASE_HOST, self::DATABASE_USER, self::DATABASE_PASSWORD);
+
+        if (!PackingList::$database_connect) {
+            echo "Unable to connect to DB: " . mysql_error(PackingList::$database_connect);
+            exit;
+        }
+          
+        if (!mysql_select_db(self::DATABASE_NAME, PackingList::$database_connect)) {
+            echo "Unable to select mydbname: " . mysql_error(PackingList::$database_connect);
+            exit;
+        }
+        $this->startTime = date("Y-m-d 10:00:00",mktime(0, 0, 0, date("m"), date("d")-1, date("Y")));
+        $this->endTime = date("Y-m-d 10:00:00");
+    }
+    
+    public function setStartTime($startTime){
+        $this->startTime = $startTime;
+    }
+    
+    public function setEndTime($endTime){
+        $this->endTime = $endTime;
+    }
+    
+    private function getSellerSell(){
+        $sql = "select o.sellerId,sum(sd.quantity) as num from qo_orders as o
+        left join qo_shipments as s on o.id = s.ordersId left join qo_shipments_detail as sd on s.id = sd.shipmentsId 
+        where s.modifiedOn between '$this->startTime' and '$this->endTime' and s.status = 'N' group by o.sellerId";
+        $result = mysql_query($sql, PackingList::$database_connect);
+        $i= 0;
+        while($row = mysql_fetch_assoc($result)){
+            $this->sellerSell[$i] = $row;
+            $i++;
+        }
+        
+    }
+    
+    private function getSellSku(){
+        $sql = "select sd.skuId,sd.quantity from qo_shipments as s left join qo_shipments_detail as sd
+        on s.id=sd.shipmentsId where s.modifiedOn between '$this->startTime' and '$this->endTime' and s.status = 'N'";
+        $result = mysql_query($sql, PackingList::$database_connect);
+        $i= 0;
+        $temp_sku = array();
+        $temp_num = array();
+        while($row = mysql_fetch_assoc($result)){
+            $temp_sku[$i] = $row['skuId'];
+            $temp_num[$i] = $row['quantity'];
+            $i++;
+        }
+        
+        foreach($temp_sku as $key=>$value){
+            if(array_key_exists($value, $this->sellSku)){
+                $this->sellSku[$value] += $temp_num[$key];
+            }else{
+                 $this->sellSku[$value] = $temp_num[$key];
+            }
+        }
+        
+    }
+    
+    private function getShipment(){
+        $sql = "select id,shipToName,shipToAddressLine1,shipToAddressLine2,shipToCity,shipToStateOrProvince,shipToPostalCode,shipToCountry
+        from qo_shipments where modifiedOn between '$this->startTime' and '$this->endTime' and status = 'N'";
+        $result = mysql_query($sql, PackingList::$database_connect);
+        
+        $i = 0;
+        while($row = mysql_fetch_assoc($result)){
+            $this->shipment[$i] = $row;
+            $sql_1 = "select sd.skuId,sd.quantity,i.galleryURL from qo_shipments_detail as sd left join qo_items as i on sd.itemId=i.id where sd.shipmentsId='".$row['id']."'";
+            $result_1 = mysql_query($sql_1, PackingList::$database_connect);
+            $j = 0;
+            while($row_1 = mysql_fetch_assoc($result_1)){
+                $this->shipment[$i]['shipmentDetail'][$j]['skuId'] = $row_1['skuId'];
+                $this->shipment[$i]['shipmentDetail'][$j]['quantity'] = $row_1['quantity'];
+                $this->shipment[$i]['shipmentDetail'][$j]['image'] = $row_1['galleryURL'];
+                $j++;
+            }
+            $i++;
+        }
+    }
+
+    private function generateFile($fileName, $content){
+        if(!file_exists(self::FILE_PATH.date("Ymd"))){
+            mkdir(self::FILE_PATH.date("Ymd"), 0777);
+        }
+        
+        $fileName = self::FILE_PATH.date("Ymd").'/packingList.html';
+        
+        if (!$handle = fopen($fileName, 'w')) {
+            echo "not open file $fileName";
+            exit;
+        }
+
+        if (fwrite($handle, $content) === FALSE) {
+            echo "not write into $fileName";
+            exit;
+        }
+                            
+        fclose($handle);
+    }
+    
+    public function getPackingList(){
+        $this->getSellerSell();
+        $this->getSellSku();
+        $this->getShipment();
+        //print_r($this->sellerSell);
+        //print_r($this->sellSku);
+        //print_r($this->shipment);
+        ob_start();
+        require("template.php");
+        $content = ob_get_contents();
+	ob_end_clean();
+        $this->generateFile('pickinglist',$content);
+    }
+    
+    public function __destruct(){
+        mysql_close(PackingList::$database_connect);
+    }
+
+
+}
+$packing_list = new PackingList();
+if(!empty($_GET)){
+    $packing_list->setStartTime($_GET['start']);
+    $packing_list->setEndTime($_GET['end']);
+}
+$packing_list->getPackingList();
+
+//http://heshuai64.3322.org/eBayBO/cron/PackingList.php?start=2009-04-22%2000:00:00&end=2009-04-23%2000:00:00
+?>
---
+++ cron/Shipment.php
@@ -0,0 +1,128 @@
+<?php
+class Shipment{
+    private static $database_connect;
+    const DATABASE_HOST = 'localhost';
+    const DATABASE_USER = 'root';
+    const DATABASE_PASSWORD = '';
+    const DATABASE_NAME = 'ebaybo';
+    private $startTime;
+    private $endTime;
+    private $complete_orders = array();
+    
+    public function __construct(){
+        Shipment::$database_connect = mysql_connect(self::DATABASE_HOST, self::DATABASE_USER, self::DATABASE_PASSWORD);
+
+        if (!Shipment::$database_connect) {
+            echo "Unable to connect to DB: " . mysql_error(Shipment::$database_connect);
+            exit;
+        }
+          
+        if (!mysql_select_db(self::DATABASE_NAME, Shipment::$database_connect)) {
+            echo "Unable to select mydbname: " . mysql_error(Shipment::$database_connect);
+            exit;
+        }
+        $this->startTime = date("Y-m-d 10:00:00",mktime(0, 0, 0, date("m"), date("d")-1, date("Y")));
+        $this->endTime = date("Y-m-d 10:00:00");
+    }
+    
+    public function setStartTime($startTime){
+        $this->startTime = $startTime;
+    }
+    
+    public function setEndTime($endTime){
+        $this->endTime = $endTime;
+    }
+    
+    private function getCompleteOrder(){
+        $sql = "select id,shippingMethod,ebayName,ebayEmail,ebayAddress1,ebayAddress2,ebayCity,ebayStateOrProvince,ebayPostalCode,ebayCountry,ebayPhone from qo_orders 
+        where status = 'P' and modifiedOn between '".$this->startTime."' and '".$this->endTime."'";
+        $result = mysql_query($sql, Shipment::$database_connect);
+        $i= 0;
+        while($row = mysql_fetch_assoc($result)){
+            $this->complete_orders[$i] = $row;
+            $sql_1 = "select skuId,skuTitle,itemId,itemTitle,quantity,barCode from qo_orders_detail where ordersId = '".$row['id']."'";
+            $result_1 = mysql_query($sql_1, Shipment::$database_connect);
+            $j = 0;
+            while($row_1 = mysql_fetch_assoc($result_1)){
+                $this->complete_orders[$i]['detail'][$j] = $row_1;
+                $j++;
+            }
+            $i++;
+        }
+    }
+    
+    private function getShipmentId(){
+        $type = 'SHA';
+        $today = date("Ym");
+        $sql = "select curType,curId from sequence where curDate='$today' and type='$type'";
+        $result = mysql_query($sql, Shipment::$database_connect);
+        $row = mysql_fetch_assoc($result);
+       
+        if($row["curId"] >=9999){
+            // A-Z 66-91
+            $curType = chr(ord($row["curType"]) + 1);
+            $sql = "update  sequence  set curId = 1,curType='$curType' where curDate='$today' and type='$type'";
+            mysql_query($sql, Shipment::$database_connect);
+        }elseif($row["curId"] < 1 || $row["curId"] == null) {
+              $sql = "insert into sequence (type,curType,curDate,curId) value ('$type','A','$today',1)";
+              mysql_query($sql, Shipment::$database_connect);
+        }else {   
+            $sql = "update sequence set curId = curId + 1 where curDate='$today' and type='$type'";
+            $result = mysql_query($sql, Shipment::$database_connect);
+        }
+       
+        $sql = "select curType,curId from sequence where curDate='$today' and type='$type'";
+        $result = mysql_query($sql, Shipment::$database_connect);
+        $row = mysql_fetch_assoc($result);
+        $shipmentId = $type.$today.$row["curType"].str_repeat("0",(4-strlen($row["curId"]))).$row["curId"];   
+        return $shipmentId;
+    }
+    
+    private function log($content){
+        file_put_contents("/export/eBayBO/log/Shipment/CreateShipment-".date("Y-m-d").".log", date("Y-m-d H:i:s")."   ".$content."\n", FILE_APPEND);
+    }
+    
+    public function createShipment(){
+        $this->getCompleteOrder();
+        print_r($this->complete_orders);
+        foreach($this->complete_orders as $orders){
+            $sql_0 = "select count(*) as num from qo_shipments where ordersId = '".$orders['id']."'";
+            $result_0 = mysql_query($sql_0, Shipment::$database_connect);
+            $row_0 = mysql_fetch_assoc($result_0);
+            if($row_0['num'] == 0){
+                $shipmentId = $this->getShipmentId();
+                $sql = "insert into qo_shipments (id,ordersId,status,shippingFeeCurrency,shippingFeeValue,shipToName,
+                shipToEmail,shipToAddressLine1,shipToAddressLine2,shipToCity,shipToStateOrProvince,shipToPostalCode,
+                shipToCountry,shipToPhoneNo,createdBy,createdOn,modifiedBy,modifiedOn) values ('".$shipmentId."','".$orders['id']."',
+                'N','".$orders['shippingFeeCurrency']."','".$orders['shippingFeeValue']."','".$orders['ebayName']."',
+                '".$orders['ebayEmail']."','".$orders['ebayAddress1']."','".$orders['ebayAddress2']."','".$orders['ebayCity']."',
+                '".$orders['ebayStateOrProvince']."','".$orders['ebayPostalCode']."','".$orders['ebayCountry']."','".$orders['ebayPhone']."',
+                'PayPal','".date("Y-m-d H:i:s")."','PayPal','".date("Y-m-d H:i:s")."')";
+                $this->log($sql);
+                $result = mysql_query($sql, Shipment::$database_connect);
+                if($result){
+                    foreach($orders['detail'] as $datail){
+                        $sql_1 = "insert into qo_shipments_detail (shipmentsId,skuId,skuTitle,itemId,itemTitle,quantity,barCode) values
+                        ('".$shipmentId."','".$datail['skuId']."','".$datail['skuTitle']."','".$datail['itemId']."','".$datail['itemTitle']."',
+                        '".$datail['quantity']."','".$datail['barCode']."')";
+                        $this->log($sql_1);
+                        $result_1 = mysql_query($sql_1, Shipment::$database_connect);
+                    }
+                }
+            }
+        }
+    }
+    
+    public function __destruct(){
+        mysql_close(Shipment::$database_connect);
+    }
+}
+
+$shipment = new Shipment();
+if(!empty($_GET['start']) && !empty($_GET['end'])){
+    $shipment->setStartTime($_GET['start']);
+    $shipment->setEndTime($_GET['end']);
+}
+$shipment->createShipment();
+//http://heshuai64.3322.org/eBayBO/cron/Shipment.php?start=2009-04-22%2000:00:00&end=2009-04-23%2000:00:00
+?>
