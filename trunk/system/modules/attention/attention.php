7937fb3cca3e39a47d58d32546cadc6139cf0446
heshuai64 <heshuai64@8678416c-2391-11de-982c-e375d58577e7>
2009-04-29 15:59:53 +0000

    attention add by seller search

--- trunk/system/modules/attention/attention.php
+++ trunk/system/modules/attention/attention.php
@@ -8,15 +8,28 @@ class QoAttention {
 	}
 	
 	public function getUnmapTransaction(){
-		$sql = "select count(*) as num from qo_transactions where createdOn between '".$_POST['start_date']."' and '".$_POST['end_date']."' and id not in (select transactionsId from qo_orders_transactions)";
-		$result = mysql_query($sql);
-		$row = mysql_fetch_assoc($result);
-		
-		$sql_1 = "select * from qo_transactions where createdOn between '".$_POST['start_date']."' and '".$_POST['end_date']."' and id not in (select transactionsId from qo_orders_transactions) limit ".$_POST['start'].",".$_POST['limit'];
-		$result_1 = mysql_query($sql_1);
-		$array = array();
-		while($row_1 = mysql_fetch_assoc($result_1)){
-			$array[] = $row_1;
+		if(!empty($_POST['payeeId'])){
+			$sql = "select count(*) as num from qo_transactions where payeeId='".$_POST['payeeId']."' and createdOn between '".$_POST['start_date']."' and '".$_POST['end_date']."' and id not in (select transactionsId from qo_orders_transactions)";
+			$result = mysql_query($sql);
+			$row = mysql_fetch_assoc($result);
+			
+			$sql_1 = "select * from qo_transactions where payeeId='".$_POST['payeeId']."' and createdOn between '".$_POST['start_date']."' and '".$_POST['end_date']."' and id not in (select transactionsId from qo_orders_transactions) limit ".$_POST['start'].",".$_POST['limit'];
+			$result_1 = mysql_query($sql_1);
+			$array = array();
+			while($row_1 = mysql_fetch_assoc($result_1)){
+				$array[] = $row_1;
+			}
+		}else{
+			$sql = "select count(*) as num from qo_transactions where createdOn between '".$_POST['start_date']."' and '".$_POST['end_date']."' and id not in (select transactionsId from qo_orders_transactions)";
+			$result = mysql_query($sql);
+			$row = mysql_fetch_assoc($result);
+			
+			$sql_1 = "select * from qo_transactions where createdOn between '".$_POST['start_date']."' and '".$_POST['end_date']."' and id not in (select transactionsId from qo_orders_transactions) limit ".$_POST['start'].",".$_POST['limit'];
+			$result_1 = mysql_query($sql_1);
+			$array = array();
+			while($row_1 = mysql_fetch_assoc($result_1)){
+				$array[] = $row_1;
+			}
 		}
 		echo json_encode(array('totalCount'=>$row['num'], 'records'=>$array));
 		mysql_free_result($result);
