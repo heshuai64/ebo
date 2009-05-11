63b397dbe7fc2136afc3ea45a8db6ee953e4466a
heshuai64 <heshuai64@8678416c-2391-11de-982c-e375d58577e7>
2009-05-11 17:06:11 +0000

    modified inventory server address

--- service.php
+++ service.php
@@ -5,6 +5,7 @@ class Service{
     const DATABASE_USER = 'root';
     const DATABASE_PASSWORD = '';
     const DATABASE_NAME = 'ebaybo';
+    const INVENTORY_SERVICE = 'http://127.0.0.1/einv2/service.php';
     
     const SHIPPED_BULK_TEMPLET = "Hi %s,<p>
             Thank you for your purchasing from us and prompt payment, your item %s has been posted to the dispatch center just now which will be sent out soon via the HongKong post regular air mail without tracking number. It normally will takes around 7 to 15 business days from the dispatch date (public holidays and weekends are not recognized as \"business days\"), please kindly wait a few days for delivery.<p>
@@ -176,7 +177,6 @@ class Service{
     }
     
     public function updateShippingMethod(){
-        $service_url = "http://127.0.0.1/einv2/service.php";
         $sql = "select id,ebayCountry from qo_orders where id = 'ORD200904A0019'";//shippingMethodStatus = 0
         $result = mysql_query($sql, Service::$database_connect);
         //var_dump($result);
@@ -196,7 +196,7 @@ class Service{
             $data_json = json_encode($data_array);
             //$sku_string = substr($sku_string, 0, -1);
             //$json_result = $this->getService($request."?skuString=".$sku_string."&country=".$row['ebayCountry']);
-            $json_result = $this->getService($service_url."?action=getShippingMethodBySku&data=".urlencode($data_json));
+            $json_result = $this->getService(self::INVENTORY_SERVICE."?action=getShippingMethodBySku&data=".urlencode($data_json));
             echo $json_result;
             $service_result = json_decode($json_result);
             var_dump($service_result);
