9ae98a81c9e7b830876bb5acf158596d1c5a9659
heshuai64 <heshuai64@8678416c-2391-11de-982c-e375d58577e7>
2009-05-19 10:05:17 +0000

    eBayListing

--- eBayListing/service.php
+++ eBayListing/service.php
@@ -6,13 +6,17 @@ class eBayListing{
     private static $database_connect;
     const DATABASE_HOST = 'localhost';
     const DATABASE_USER = 'root';
-    const DATABASE_PASSWORD = '5333533';
+    const DATABASE_PASSWORD = '';
     const DATABASE_NAME = 'ebaylisting';
     const GATEWAY_SOAP = 'https://api.sandbox.ebay.com/wsapi';
     private $startTime;
     private $endTime;
+    private $session;
     
     public function __construct(){
+	session_start();
+	$_SESSION['account_id'] = 1;
+	
         eBayListing::$database_connect = mysql_connect(self::DATABASE_HOST, self::DATABASE_USER, self::DATABASE_PASSWORD);
 
         if (!eBayListing::$database_connect) {
@@ -27,6 +31,16 @@ class eBayListing{
             exit;
         }
        
+	$sql = "select token from account where id = '".$_SESSION['account_id']."'";
+	$result = mysql_query($sql, eBayListing::$database_connect);
+	$row = mysql_fetch_assoc($result);
+	
+	
+	$sql_1 = "select p.host,p.port from proxy as p left join account_to_proxy as atp on p.id = atp.proxy_id where atp.account_id = '".$_SESSION['account_id']."'";
+	$result_1 = mysql_query($sql_1, eBayListing::$database_connect);
+	$row_1 = mysql_fetch_assoc($result_1);
+	
+	$this->session = $this->configEbay($row['token'], $row_1['host'], $row_1['port']);
     }
     
     private function configEbay($token='', $proxy_host='', $proxy_port=''){
@@ -53,12 +67,11 @@ class eBayListing{
     }
     
     
-    public function GetCategories(){
-        $session = $this->configEbay($dev, $app, $cert, $token, $proxy_host, $proxy_port);
+    public function getCategories($siteId){
         try {
-                $client = new eBaySOAP($session);
+                $client = new eBaySOAP($this->session);
 
-                $CategorySiteID = 100;
+                $CategorySiteID = $siteId;
                 $Version = '607';
                 $DetailLevel = "ReturnAll";
              
@@ -75,10 +88,43 @@ class eBayListing{
         }
     }
     
+    public function checkCategoriesVersion($siteId, $version){
+        try {
+                $client = new eBaySOAP($this->session);
+
+                $CategorySiteID = $siteId;
+                $Version = '607';
+                $params = array('Version' => $Version, 'CategorySiteID' => $CategorySiteID);
+                $results = $client->GetCategories($params);
+                //----------   debug --------------------------------
+                print "Request: \n".$client->__getLastRequest() ."\n";
+                print "Response: \n".$client->__getLastResponse()."\n";
+		return $results;
+                exit;
+        } catch (SOAPFault $f) {
+                print $f; // error handling
+        }
+    }
+    
+    
+    public function getAllCategories(){
+	$sql = "select id,name,version from site where status = 1";
+	$result = mysql_query($sql, eBayListing::$database_connect);
+	while ($row = mysql_fetch_assoc($result)){
+	    $result = $this->checkCategoriesVersion($row['id'], $row['version']);
+	    if($result){
+		$this->getCategories($row['id']);
+	    }else{
+		echo $row['name']." categories no change<br>";
+	    }
+	}
+    }
     
     public function __destruct(){
         mysql_close(eBayListing::$database_connect);
     }
 }
 
+$service = new eBayListing();
+$service->getAllCategories();
 ?>
