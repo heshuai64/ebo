069dfc187541e0266882bb2e5ad20af6ae4c519f
heshuai64 <heshuai64@8678416c-2391-11de-982c-e375d58577e7>
2009-05-26 11:57:10 +0000

    item edit page

---
+++ trunk/eBayListing/create_new_item.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>eBay Listing</title>
+    <link rel="stylesheet" type="text/css" href="../../Ext/2.2/resources/css/ext-all.css" />
+    <script src="../../Ext/2.2/adapter/ext/ext-base.js"></script>
+    <script src="../../Ext/2.2/ext-all.js"></script>
+    <script src="item.js"></script>
+    <style type="text/css">
+	html, body {
+            font:normal 12px verdana;
+            margin:0;
+            padding:0;
+            border:0 none;
+            overflow:hidden;
+            height:100%;
+        }
+	.my-fieldset .x-fieldset-header .x-fieldset-header-text {float: left; padding: 5px 5px 0 0;}
+	.my-fieldset .x-fieldset-header .x-form-field-wrap {float: left;}	
+    </style>
+<body>
+    <!-- Insert your content here -->
+</body>
+</html>
