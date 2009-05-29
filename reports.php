29d359e5e89ebf67d205b623c9bfd5c83e066920
heshuai64 <heshuai64@8678416c-2391-11de-982c-e375d58577e7>
2009-05-29 04:28:55 +0000

    getModelBySkuId

--- reports.php
+++ reports.php
@@ -5,6 +5,7 @@ class Reports{
     const DATABASE_USER = 'root';
     const DATABASE_PASSWORD = '5333533';
     const DATABASE_NAME = 'ebaybo';
+    const INVENTORY_SERVICE = 'http://192.168.1.169:8080/inventory/service.php';
     
     public function __construct(){
         Reports::$database_connect = mysql_connect(self::DATABASE_HOST, self::DATABASE_USER, self::DATABASE_PASSWORD);
@@ -21,6 +22,46 @@ class Reports{
         
     }
     
+    private function log($file_name, $data){
+        file_put_contents("/export/eBayBO/log/".$file_name."-".date("Y-m-d").".html", $data, FILE_APPEND);  
+    }
+    
+    private function get($request){
+        
+	$session = curl_init($request);
+		
+	curl_setopt($session, CURLOPT_HEADER, true);
+	curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+	
+	$response = curl_exec($session);
+	
+	curl_close($session);
+	
+	$status_code = array();
+	preg_match('/\d\d\d/', $response, $status_code);
+	
+	switch( $status_code[0] ) {
+		case 200:
+		    if ($result = strstr($response, 'model:')) {
+			return $result;
+		    }
+		    break;
+		case 503:
+			die('Your call to Web Services failed and returned an HTTP status of 503. That means: Service unavailable. An internal problem prevented us from returning data to you.');
+			break;
+		case 403:
+			die('Your call to Web Services failed and returned an HTTP status of 403. That means: Forbidden. You do not have permission to access this resource, or are over your rate limit.');
+			break;
+		case 400:
+			die('Your call to Web Services failed and returned an HTTP status of 400. That means:  Bad request. The parameters passed to the service did not match as expected. The exact error is returned in the XML response.');
+			break;
+		default:
+			die('Your call to Web Services returned an unexpected HTTP status of:' . $status_code[0]);
+			return false;
+	}
+
+    }
+    
     public function skuSellReport($seller_id, $start_date, $end_date){
         require ("class/class-excel-xml.inc.php");
         if(empty($seller_id)){
@@ -31,10 +72,15 @@ class Reports{
         //echo $sql;
         $result = mysql_query($sql, Reports::$database_connect);
         $data = array();
-        $data[0] = array('Seller', 'SKU', 'Quantity');
+        $data[0] = array('Seller', 'SKU', 'MODEL', 'Quantity');
         $i = 1;
         while($row = mysql_fetch_assoc($result)){
-            $data[$i] = $row;
+            $service_result = $this->get(self::INVENTORY_SERVICE.'?action=getModelBySkuId&skuId='.urlencode($row['skuId']));
+            $this->log('getModelBySkuId.html','skuId:'.$row['skuId'].', return:'.$service_result);
+            $data[$i]['sellerId'] = $row['sellerId'];
+            $data[$i]['skuId'] = $row['skuId'];
+            $data[$i]['model'] = substr($service_result, 6);
+            $data[$i]['quantity'] = $row['quantity'];
             $i++;
         }
         //var_dump($data);
