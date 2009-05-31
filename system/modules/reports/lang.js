f43adb315afc75701da8b0329cd29ebbc7cf6c5e
heshuai64 <heshuai64@8678416c-2391-11de-982c-e375d58577e7>
2009-05-31 16:49:53 +0000

    Sales report

--- system/modules/reports/lang.js
+++ system/modules/reports/lang.js
@@ -2,6 +2,8 @@ var lang = {
     reports:{
         window_title:'Statistical Reports',
         sell_sell:'Sku Sell',
+        sales_report:'Sales Report',
+        search_sales_report_window_title:'Search Seller Sales',
         search_sku_sell_window_title: 'Search Sku Sell',
         seller_id: 'Sell Account',
         start_date: 'Start Date',
