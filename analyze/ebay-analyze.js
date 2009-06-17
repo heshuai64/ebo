35d2454f8b44e876913b0a3a91409be315d3833b
heshuai64 <heshuai64@8678416c-2391-11de-982c-e375d58577e7>
2009-06-17 17:40:24 +0000

    modified analyze

--- analyze/ebay-analyze.js
+++ analyze/ebay-analyze.js
@@ -3,14 +3,14 @@ Ext.onReady(function(){
     
     //http://open.api.ebay.com/shopping?callname=FindItems&callbackname=com.ebay.shoppingservice.Shopping.findItemsClosure0&responseencoding=JSON&callback=true&version=607&appId=eBayAPID-73f4-45f2-b9a3-c8f6388b38d8&QueryKeywords=ipod&MaxEntries=1&client=js
     //http://open.api.ebay.com/shopping?callname=GetSingleItem&callbackname=com.ebay.shoppingservice.Shopping.getSingleItemClosure1&responseencoding=JSON&callback=true&version=607&appId=eBayAPID-73f4-45f2-b9a3-c8f6388b38d8&ItemID=390059143734&IncludeSelector=Details%2CShippingCosts&client=js
-    
+    //http://open.api.ebay.com/shopping?callname=FindItemsAdvanced&callbackname=com.ebay.shoppingservice.Shopping.findItemsAdvancedClosure0&responseencoding=JSON&callback=true&version=607&appId=eBayAPID-73f4-45f2-b9a3-c8f6388b38d8&QueryKeywords=Battery&ItemSort=CurrentBid&SellerID(0)=%20libra.studio&SellerID(1)=easybattery&MaxEntries=10&client=js
     com.ebay.widgets.needs({
         baseUrl: 'http://w-1.ebay.com/js/607/min/',
         files: ['FindItemsAdvanced.js', 'GetSingleItem.js'],
 	resources: [].concat(com.ebay.shoppingservice.Shopping.getSingleItem, com.ebay.shoppingservice.Shopping.findItemsAdvanced),
         callback: function() {
        
-            console.log("render grid");
+            //console.log("render grid");
             var reader = new Ext.data.ArrayReader({}, [
                 {name: 'itemID'},
                 {name: 'userID'},
@@ -37,7 +37,8 @@ Ext.onReady(function(){
             }
             
             function renderShippingServiceCost(v, p, r){
-                if(v.substr(2) == "0"){
+		//console.log(v.substr(3));
+                if(v.substr(3) == "0" || v.substr(3) == 0){
                     return String.format('<font color="red">{0}</font>', v);
                 }else{
                     return String.format('<font color="green">{0}</font>', v);
@@ -54,9 +55,9 @@ Ext.onReady(function(){
                 columns:[
                     {header: "Image", width: 100, align: 'center', sortable: true, dataIndex: 'galleryURL', renderer: renderImage},
                     {header: "Seller ID", width: 80, align: 'center', sortable: true, dataIndex: 'userID'},
-                    {header: "Title", width: 350, align: 'center', sortable: true, dataIndex: 'title'},
-                    {header: "Buy It Now", width: 80, align: 'center', sortable: true, dataIndex: 'buyItNowAvailable'},
-                    {header: "Price", width: 50, align: 'center', sortable: true, dataIndex: 'currentPrice'},
+                    {header: "Title", width: 320, align: 'center', sortable: true, dataIndex: 'title'},
+                    {header: "Buy It Now", width: 70, align: 'center', sortable: true, dataIndex: 'buyItNowAvailable'},
+                    {header: "Price", width: 55, align: 'center', sortable: true, dataIndex: 'currentPrice'},
                     {header: "shipping Cost", width: 100, align: 'center', sortable: true, dataIndex: 'shippingServiceCost', renderer: renderShippingServiceCost},
                     {header: "Quantity", width: 60, align: 'center', sortable: true, dataIndex: 'quantity'},
                     {header: "Sold Quantity", width: 80, align: 'center', sortable: true, dataIndex: 'quantitySold'},
@@ -82,11 +83,27 @@ Ext.onReady(function(){
                         xtype: 'textfield',
                         width: 200
                     },'-',{
+			xtype: 'tbtext',
+                        text: 'End time from:'
+		    },{
+			id: 'from',
+			name: 'from',
+			xtype: 'datefield',
+			format : 'Y-m-d'
+		    },'-',{
+			xtype: 'tbtext',
+                        text: 'End time to:'
+		    },{
+			id: 'to',
+			name: 'to',
+			xtype: 'datefield',
+			format : 'Y-m-d'
+		    },{
                         text: 'Submit',
                         handler: function(){
                             function findItemsAdvancedSuccess(data) {
-                                console.log("findItemsAdvancedSuccess");
-                                console.log(data);
+                                //console.log("findItemsAdvancedSuccess");
+                                //console.log(data);
                                 
                                 var i = 0;
                                 var itemArray = new Array();
@@ -94,8 +111,8 @@ Ext.onReady(function(){
                                 
                                 //---------------------------------  getSingleItemSuccess  -----------------------------------------------------
                                 var getSingleItemSuccess = function(data){
-                                    console.log("getSingleItemSuccess (" + i + ")");
-                                    console.log(data);
+                                    //console.log("getSingleItemSuccess (" + i + ")");
+                                    //console.log(data);
                                     var item = new Array();
                                     item[0] = data.item.itemID
                                     item[1] = data.item.seller.userID;
@@ -134,11 +151,10 @@ Ext.onReady(function(){
                                     console.log(data);
                                     */
                                     i++;
-                                    console.log(i);
-                                    console.log(itemArray);
                                     
                                     if(i == itemTotalNum){
-                                        console.log("store load data.");
+					//console.log(itemArray);
+                                        //console.log("store load data.");
                                         store.loadData(itemArray);
                                     }
                                 }
@@ -150,7 +166,7 @@ Ext.onReady(function(){
                         
                                 if(Ext.isArray(data.searchResult[0].itemArray.item)){
                                     itemTotalNum = data.searchResult[0].itemArray.item.length;
-                                    console.log("item is array, count " + itemTotalNum);
+                                    //console.log("item is array, count " + itemTotalNum);
                                     for(j in data.searchResult[0].itemArray.item){
                                         if(!Ext.isEmpty(data.searchResult[0].itemArray.item[j].itemID)){
                                             var config = new com.ebay.shoppingservice.ShoppingConfig({appId: 'eBayAPID-73f4-45f2-b9a3-c8f6388b38d8'});
@@ -181,7 +197,7 @@ Ext.onReady(function(){
                             
                             var config = new com.ebay.shoppingservice.ShoppingConfig({appId: 'eBayAPID-73f4-45f2-b9a3-c8f6388b38d8'});
                             var shopping = new com.ebay.shoppingservice.Shopping(config);
-                            var request = new com.ebay.shoppingservice.FindItemsAdvancedRequestType({QueryKeywords: Ext.getCmp('keyword').getValue(), SellerID: Ext.getCmp('seller').getValue(), MaxEntries: 10});
+                            var request = new com.ebay.shoppingservice.FindItemsAdvancedRequestType({QueryKeywords: Ext.getCmp('keyword').getValue(), SellerID: Ext.getCmp('seller').getValue(), MaxEntries: 10, EndTimeFrom: Date.parseDate(Ext.getCmp('from').getValue(), "Y-m-d"), EndTimeTo: Date.parseDate(Ext.getCmp('to').getValue(), "Y-m-d")});
                             var callback = new com.ebay.shoppingservice.ShoppingCallback({success: findItemsAdvancedSuccess, failure: findItemsAdvancedFailure});
                             shopping.findItemsAdvanced(request, callback);
                                 
