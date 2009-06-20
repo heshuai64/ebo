9c7b1ed749a371c5d91f8130a1ecb173d50e2fd3
heshuai64 <heshuai64@8678416c-2391-11de-982c-e375d58577e7>
2009-06-20 04:19:55 +0000

    sales report

---
+++ skuSalesChart.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta http-equiv="PRAGMA" content="NO-CACHE">
+        <meta http-equiv="CACHE-CONTROL" content="NO-CACHE">
+        <meta http-equiv="EXPIRES" content="-1">
+        
+        <title>Sku Sales Chart</title>
+        
+        <!-- EXT JS LIBRARY -->
+        <link rel="stylesheet" type="text/css" href="../Ext/2.2/resources/css/ext-all.css" />
+        <script src="../Ext/2.2/adapter/ext/ext-base.js"></script>
+        <script src="../Ext/2.2/ext-all.js"></script>
+        <script type='text/javascript' src='http://www.google.com/jsapi'></script>
+        <script type="text/javascript">
+            var week = "<?=$_GET['week']?>";
+            var sellerId = "<?=$_GET['sellerId']?>";
+        </script>
+</head>
+
+<body>
+    <div id='chart_div'></div>
+</body>
+</html>
